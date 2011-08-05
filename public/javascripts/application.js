97922ca61d80e82db7a830a0e4cef76466bd4db6
Larry Karnowski <larry@hickorywind.org>
2011-08-05 16:15:25 -0400

    added more songs to the playlist

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
- $("#jquery_jplayer_1").jPlayer({
-  ready: function () {
-   $(this).jPlayer("setMedia", {
-    mp3: "/media/rick.mp3",
-   });
-  },
-  swfPath: "/swfs",
-  supplied: "mp3"
- });
+  var RICK = { mp3: "/media/rick.mp3" };
+  var BLUEGRASS = { mp3: "/media/Going Back To Mechanicsville.mp3" };
+  
+  $("#jquery_jplayer_1").jPlayer({
+    preload: "auto",
+    swfPath: "/swfs",
+    supplied: "mp3",
+
+    ready: function() {
+      $(this).jPlayer("setMedia", RICK);
+    }
+  });
+  
+  $("li#rickroll").click(function() {
+    console.log("clicked rickroll");
+    
+    $("#jquery_jplayer_1").jPlayer("setMedia", RICK);
+    $("#jquery_jplayer_1").jPlayer("play");
+  });
+
+  $("li#bluegrass").click(function() {
+    console.log("clicked bluegrass"); 
+
+    $("#jquery_jplayer_1").jPlayer("setMedia", BLUEGRASS);
+    $("#jquery_jplayer_1").jPlayer("play");
+  });
+
 });
