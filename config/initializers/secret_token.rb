e1bc3a209a0269eec8096a3514ac608a0a47cc87
Larry Karnowski <larry@hickorywind.org>
2011-12-09 15:46:11 -0500

    freshly-generated Rails 3.1.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webpod::Application.config.secret_token = '2c744302e3d0d411636fe8b5285d401f2a90f7efa28440175f949952bcc33fde4be97207641ddd44c5f401aeae3cc20dbe979788f976dd68f85070ef8cf66dbd'
